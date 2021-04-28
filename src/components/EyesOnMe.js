55f8727ded749e5bfcce542956c8fd051136ed99
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-04-28 15:58:55 -0700

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component{
+	render () {
+		const focusing = () => {
+			console.log('Good!')
+		}
+
+		const blur = () => {
+			console.log('Hey! Eyes on me!')
+		}
+	return (
+		<div>
+			<button onFocus={focusing} onBlur={blur}></button>
+		</div>
+	)
+	}
+	
+}
+
+export default EyesOnMe
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+import React from 'react'
+
+class Keypad extends React.Component{
+	render () {
+
+		const keyUp = () => {
+			console.log('Entering password...')
+		}
+	return (
+		<div>
+			<input type="password" onKeyUp={keyUp}></input>
+		</div>
+	)
+	}
+	
+}
+
+export default Keypad
